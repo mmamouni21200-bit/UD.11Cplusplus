3c5aba9927b68cfa1f4404e550a8bedf8d190797
mohamam <mmamouni21200@iesjoanramis.org>
2026-02-23 09:40:48 +0100

    ahora

--- Ex6.cpp
+++ Ex6.cpp
@@ -1,53 +1,74 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
+int menu() {
     int opcio;
     double a, b;
-    
-    cout << "=== CALCULADORA ===\n";
+
+    cout << "\n=== CALCULADORA ===\n";
     cout << "1. Suma\n";
     cout << "2. Resta\n";
     cout << "3. Multiplicacio\n";
     cout << "4. Divisio\n";
     cout << "5. Modul (nombres enters)\n";
+    cout << "6. Sortir\n";
     cout << "Tria una opcio: ";
     cin >> opcio;
-    
-    cout << "Introdueix el primer nombre: ";
-    cin >> a;
-    cout << "Introdueix el segon nombre: ";
-    cin >> b;
-    
+
+    if(opcio >= 1 && opcio <= 5) {
+        cout << "Introdueix el primer nombre: ";
+        cin >> a;
+        cout << "Introdueix el segon nombre: ";
+        cin >> b;
+    }
+
     switch(opcio) {
         case 1:
             cout << "Resultat: " << a + b << endl;
             break;
-            
+
         case 2:
             cout << "Resultat: " << a - b << endl;
             break;
-            
+
         case 3:
             cout << "Resultat: " << a * b << endl;
             break;
-            
+
         case 4:
-            if(num2 != 0) {
+            if(b != 0) {
                 cout << "Resultat: " << a / b << endl;
             } else {
                 cout << "Error: No es pot dividir per zero!" << endl;
             }
             break;
-            
+
         case 5:
-            // Convertim a enters per al modul
-            cout << "Resultat: " << (int)a % (int)b << endl;
+            if((int)b != 0) {
+                cout << "Resultat: " << (int)a % (int)b << endl;
+            } else {
+                cout << "Error: No es pot fer modul amb zero!" << endl;
+            }
             break;
-            
+
+        case 6:
+            cout << "Sortint del programa...\n";
+            break;
+
         default:
-            cout << "Opcio no valida!" << endl;
+            cout << "Opcio no valida!\n";
     }
-    
+
+    return opcio;
+}
+
+int main() {
+    int op;
+
+    do {
+        op = menu();
+    } while(op != 6);
+
     return 0;
 }
